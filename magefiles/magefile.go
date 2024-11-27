d9015d37b38026545a977613063055e5df0c8523
Armin Abfalterer <armin.abfalterer@united-security-providers.ch>
2024-11-27 15:45:06 +0100

    fix docker compose call

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -24,22 +24,22 @@ func Build() error {
 	return sh.RunV("go", "build", "-o", "plugin.so", "-buildmode=c-shared", "./plugin")
 }
 
-// RunExample spins up the test environment, access at http://localhost:8080. Requires docker-compose.
+// RunExample spins up the test environment, access at http://localhost:8080. Requires docker compose.
 func RunExample() error {
-	return sh.RunV("docker-compose", "--file", "example/docker-compose.yml", "up", "-d")
+	return sh.RunV("docker", "compose", "--file", "example/docker-compose.yml", "up", "-d")
 }
 
-// TeardownExample tears down the test environment. Requires docker-compose.
+// TeardownExample tears down the test environment. Requires docker compose.
 func TeardownExample() error {
-	return sh.RunV("docker-compose", "--file", "example/docker-compose.yml", "down")
+	return sh.RunV("docker", "compose", "--file", "example/docker-compose.yml", "down")
 }
 
-// E2e runs e2e tests with a built plugin against the example deployment. Requires docker-compose.
+// E2e runs e2e tests with a built plugin against the example deployment. Requires docker compose.
 func E2e() error {
-	if err := sh.RunV("docker-compose", "--file", "e2e/docker-compose.yml", "build", "--pull"); err != nil {
+	if err := sh.RunV("docker", "compose", "--file", "e2e/docker-compose.yml", "build", "--pull"); err != nil {
 		return err
 	}
-	return sh.RunV("docker-compose", "--file", "e2e/docker-compose.yml", "up", "--abort-on-container-exit", "tests")
+	return sh.RunV("docker", "compose", "--file", "e2e/docker-compose.yml", "up", "--abort-on-container-exit", "tests")
 }
 
 // Doc runs godoc, access at http://localhost:6060
@@ -47,13 +47,13 @@ func Doc() error {
 	return sh.RunV("go", "run", "golang.org/x/tools/cmd/godoc@latest", "-http=:6060")
 }
 
-// Ftw runs ftw tests with a built plugin and Envoy. Requires docker-compose.
+// Ftw runs ftw tests with a built plugin and Envoy. Requires docker compose.
 func Ftw() error {
-	if err := sh.RunV("docker-compose", "--file", "ftw/docker-compose.yml", "build", "--pull"); err != nil {
+	if err := sh.RunV("docker", "compose", "--file", "ftw/docker-compose.yml", "build", "--pull"); err != nil {
 		return err
 	}
 	defer func() {
-		_ = sh.RunV("docker-compose", "--file", "ftw/docker-compose.yml", "down", "-v")
+		_ = sh.RunV("docker", "compose", "--file", "ftw/docker-compose.yml", "down", "-v")
 	}()
 	env := map[string]string{
 		"FTW_CLOUDMODE": os.Getenv("FTW_CLOUDMODE"),
@@ -67,5 +67,5 @@ func Ftw() error {
 	if os.Getenv("MEMSTATS") == "true" {
 		task = "ftw-memstats"
 	}
-	return sh.RunWithV(env, "docker-compose", "--file", "ftw/docker-compose.yml", "run", "--rm", task)
+	return sh.RunWithV(env, "docker", "compose", "--file", "ftw/docker-compose.yml", "run", "--rm", task)
 }
