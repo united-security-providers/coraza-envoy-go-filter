d8461205d0d0c64039b9f42e0fc13576a2d6dfdc
Dominik Inauen <dominik.inauen@united-security-providers.ch>
2025-04-11 08:41:52 +0200

    add comments to run one test only

--- ftw/tests.sh
+++ ftw/tests.sh
@@ -32,4 +32,6 @@ FTW_CLOUDMODE=${FTW_CLOUDMODE:-false}
 
 FTW_INCLUDE=$([ "${FTW_INCLUDE}" == "" ] && echo "" || echo "-i ${FTW_INCLUDE}")
 
-/ftw run -d ./coreruleset/tests/regression/tests --config ftw.yml --overrides overrides.yml --read-timeout=10s --cloud=$FTW_CLOUDMODE $FTW_INCLUDE  || exit 1
+# add --fail-fast to exit on first failed test
+# to run only one test add -i <test> e.g. -i 920100-1
+/ftw run -d ./coreruleset/tests/regression/tests/ --config ftw.yml --overrides overrides.yml --read-timeout=10s --cloud=$FTW_CLOUDMODE $FTW_INCLUDE  || exit 1
