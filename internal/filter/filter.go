1402eb813b3e79f8d4046ec19fbd23364484a22b
kabbohus <hussein.kabbout@united-security-providers.ch>
2026-04-01 09:41:36 +0200

    Check for existence of port suffix before appending

--- internal/filter/filter.go
+++ internal/filter/filter.go
@@ -309,28 +309,28 @@ func (f *Filter) initializeTx(logger logging.Logger, headerMap api.RequestHeader
 		xReqId = ""
 	}
 	waf := f.Config.WafMaps[f.Config.DefaultDirective]
+	wafFound := false
 	ruleName, ok := f.Config.HostDirectiveMap[host]
 	if ok {
-		logger.Debug("using host configuration for tx", "waf", ruleName)
+		wafFound = true
 		waf = f.Config.WafMaps[ruleName]
+	} else if headerMap.Scheme() == "https" && !strings.HasSuffix(host, ":443") {
+		ruleName, ok = f.Config.HostDirectiveMap[host+":443"]
+		if ok {
+			wafFound = true
+			waf = f.Config.WafMaps[ruleName]
+		}
+	} else if headerMap.Scheme() == "http" && !strings.HasSuffix(host, ":80") {
+		ruleName, ok = f.Config.HostDirectiveMap[host+":80"]
+		if ok {
+			wafFound = true
+			waf = f.Config.WafMaps[ruleName]
+		}
+	}
+	if wafFound {
+		logger.Debug("using host configuration for tx", "waf", ruleName)
 	} else {
-		ruleName := ""
-		if headerMap.Scheme() == "https" {
-			ruleName, ok = f.Config.HostDirectiveMap[host+":443"]
-			if ok {
-				waf = f.Config.WafMaps[ruleName]
-			}
-		} else if headerMap.Scheme() == "http" {
-			ruleName, ok = f.Config.HostDirectiveMap[host+":80"]
-			if ok {
-				waf = f.Config.WafMaps[ruleName]
-			}
-		}
-		if ruleName == "" {
-			logger.Debug("using default host configuration for tx", "waf", f.Config.DefaultDirective)
-		} else {
-			logger.Debug("using host configuration for tx", "waf", ruleName)
-		}
+		logger.Debug("using default host configuration for tx", "waf", f.Config.DefaultDirective)
 	}
 	// the ID of the transaction is set to the ID of the request
 	// see errorCallback() in parse.go for more details
