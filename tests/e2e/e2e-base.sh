5a50032abe0c42da5457f67465dfafb8299a195a
daum3ns <23260410+daum3ns@users.noreply.github.com>
2026-04-22 18:09:59 +0200

    add testcases for per virtual host configuration
    Signed-off-by: daum3ns <23260410+daum3ns@users.noreply.github.com>

--- tests/e2e/e2e-base.sh
+++ tests/e2e/e2e-base.sh
@@ -32,7 +32,7 @@ echo "#                  E2E BASE TESTS                  #"
 echo "####################################################"
 
 step=1
-total_steps=24
+total_steps=28
 
 ## Testing that basic coraza phases are working
 
@@ -43,26 +43,27 @@ wait_for_service "${health_url}" 15
 # Testing envoy container reachability with an unfiltered GET request
 ((step+=1))
 echo "[${step}/${total_steps}] (onRequestheaders) Testing true negative request"
-wait_for_service "${envoy_url_echo}?arg=arg_1" 20
+#wait_for_service "${envoy_url_echo}?arg=arg_1" 20
+check_status "${envoy_url_echo}?arg=arg_1" 200 -H 'Host: foo.example.com'
 
 # Testing filtered request
 ((step+=1))
 echo "[${step}/${total_steps}] (onRequestheaders) Testing true positive custom rule"
-check_status "${envoy_url_filtered}" 403
+check_status "${envoy_url_filtered}" 403 -H 'Host: foo.example.com'
 # This test ensures the response body is empty on interruption. Specifically this makes
 # sure no body is returned although actionContinue is passed in phase 3 & 4.
 # See https://github.com/corazawaf/coraza-proxy-wasm/pull/126
-check_body "${envoy_url_filtered}" true
+check_body "${envoy_url_filtered}" true -H 'Host: foo.example.com'
 
 # Testing body true negative
 ((step+=1))
 echo "[${step}/${total_steps}] (onRequestBody) Testing true negative request (body)"
-check_status "${envoy_url_post}" 200 -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data "${tueNegativeBodyPayload}"
+check_status "${envoy_url_post}" 200 -X POST -H 'Host: foo.example.com' -H 'Content-Type: application/x-www-form-urlencoded' --data "${tueNegativeBodyPayload}"
 
 # Testing body detection
 ((step+=1))
 echo "[${step}/${total_steps}] (onRequestBody) Testing true positive request (body)"
-check_status "${envoy_url_post}" 403 -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data "${truePositiveBodyPayload}"
+check_status "${envoy_url_post}" 403 -X POST -H 'Host: foo.example.com' -H 'Content-Type: application/x-www-form-urlencoded' --data "${truePositiveBodyPayload}"
 
 # Testing body detection when reaching SecRequestBodyLimit (ProcessPartial)
 # It's important that the pattern triggering the rule is within SecRequestBodyLimit
@@ -70,7 +71,7 @@ check_status "${envoy_url_post}" 403 -X POST -H 'Content-Type: application/x-www
 # SecRequestBodyLimit is set to 40 so it includes the payload
 ((step+=1))
 echo "[${step}/${total_steps}] (onRequestBody) Testing true positive request (body) when inside SecRequestBodyLimit"
-check_status "${envoy_url_post}" 403 -X POST -H 'Content-Type: application/x-www-form-urlencoded' -H 'Host: bar.example.com' --data "prefix is 20 bytes ${truePositiveBodyPayload} suffix is 20 bytes"
+check_status "${envoy_url_post}" 403 -X POST  -H 'Content-Type: application/x-www-form-urlencoded' -H 'Host: bar.example.com' --data "prefix is 20 bytes ${truePositiveBodyPayload} suffix is 20 bytes"
 
 # Testing body detection when reaching SecRequestBodyLimit (ProcessPartial)
 # In this test the pattern triggering the rule is NOT within SecRequestBodyLimit
@@ -90,22 +91,22 @@ check_status "${envoy_url_post}" 413 -X POST -H 'Content-Type: application/x-www
 # Testing response headers detection
 ((step+=1))
 echo "[${step}/${total_steps}] (onResponseHeaders) Testing true positive"
-check_status "${envoy_url_filtered_resp_header}" 403
+check_status "${envoy_url_filtered_resp_header}" 403 -H 'Host: foo.example.com'
 
 # Testing response body true negative
 ((step+=1))
 echo "[${step}/${total_steps}] (onResponseBody) Testing true negative"
-check_body "${envoy_url_post}" false -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data "${trueNegativeBodyPayloadForResponseBody}"
+check_body "${envoy_url_post}" false -X POST -H 'Host: foo.example.com' -H 'Content-Type: application/x-www-form-urlencoded' --data "${trueNegativeBodyPayloadForResponseBody}"
 
 # Testing response body detection
 ((step+=1))
 echo "[${step}/${total_steps}] (onResponseBody) Testing true positive"
-check_body "${envoy_url_post}" true -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data "${truePositiveBodyPayloadForResponseBody}"
+check_body "${envoy_url_post}" true -X POST -H 'Host: foo.example.com' -H 'Content-Type: application/x-www-form-urlencoded' --data "${truePositiveBodyPayloadForResponseBody}"
 
 # Testing status code is correct on response body detection
 ((step+=1))
 echo "[${step}/${total_steps}] (onResponseBody) Testing true positive status is correct"
-check_status "${envoy_url_post}" 403 -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data "${truePositiveBodyPayloadForResponseBody}"
+check_status "${envoy_url_post}" 403 -X POST -H 'Host: foo.example.com' -H 'Content-Type: application/x-www-form-urlencoded' --data "${truePositiveBodyPayloadForResponseBody}"
 
 # Testing response body detection when reaching SecResponseBodyLimit (ProcessPartial)
 # It's important that the malicious payload is detectable within SecResponseBodyLimit
@@ -135,32 +136,32 @@ check_status "${envoy_url_unfiltered}/bytes/80" 500 -H 'Host: baz.example.com'
 # Testing XSS detection during phase 1
 ((step+=1))
 echo "[${step}/${total_steps}] Testing XSS detection at request headers"
-check_status "${envoy_url_echo}?arg=<script>alert(0)</script>" 403
+check_status "${envoy_url_echo}?arg=<script>alert(0)</script>" 403 -H 'Host: foo.example.com'
 
 # Testing SQLI detection during phase 2
 ((step+=1))
 echo "[${step}/${total_steps}] Testing SQLi detection at request body"
-check_status "${envoy_url_post}" 403 -X POST --data "1%27%20ORDER%20BY%203--%2B"
+check_status "${envoy_url_post}" 403 -H 'Host: foo.example.com' -X POST --data "1%27%20ORDER%20BY%203--%2B"
 
 # Triggers a CRS scanner detection rule (913100)
 ((step+=1))
 echo "[${step}/${total_steps}] (onRequestBody) Testing CRS rule 913100"
-check_status "${envoy_url_echo}" 403 --user-agent "gobuster/3.2.0 (X11; U; Linux i686; en-US; rv:1.7)" -H "Host: localhost" -H "Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"
+check_status "${envoy_url_echo}" 403 -H 'Host: foo.example.com' --user-agent "gobuster/3.2.0 (X11; U; Linux i686; en-US; rv:1.7)" -H "Host: localhost" -H "Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"
 
 # True negative GET request with an usual user-agent
 ((step+=1))
 echo "[${step}/${total_steps}] True negative GET request with user-agent"
-check_status "${envoy_url_echo}" 200 --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+check_status "${envoy_url_echo}" 200 -H 'Host: foo.example.com' --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
 
 
 ## Testing per route configuration
 ((step+=1))
 echo "[${step}/${total_steps}] Testing per route config true negative"
-check_status "${envoy_url_unfiltered}/other-waf" 404
+check_status "${envoy_url_unfiltered}/other-waf" 404 -H 'Host: foo.example.com'
 
 ((step+=1))
 echo "[${step}/${total_steps}] Testing other wafs do not block per route config true negative"
-check_status "${envoy_url_unfiltered}/other-waf/admin" 404
+check_status "${envoy_url_unfiltered}/other-waf/admin" 404 -H 'Host: foo.example.com'
 
 ((step+=1))
 echo "[${step}/${total_steps}] Testing other waf does not block per route config even if host matches true negative"
@@ -168,11 +169,29 @@ check_status "${envoy_url_unfiltered}/other-waf/admin" 404 -H 'Host: foo.example
 
 ((step+=1))
 echo "[${step}/${total_steps}] Testing per route config true positive"
-check_status "${envoy_url_unfiltered}/other-waf/other-admin" 403
+check_status "${envoy_url_unfiltered}/other-waf/other-admin" 403 -H 'Host: foo.example.com'
 
 ((step+=1))
 echo "[${step}/${total_steps}] Testing per route true positive request body"
-check_status "${envoy_url_unfiltered}/other-waf" 403 -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data "evilpayload"
+check_status "${envoy_url_unfiltered}/other-waf" 403 -X POST -H 'Host: foo.example.com' -H 'Content-Type: application/x-www-form-urlencoded' --data "evilpayload"
+
+# Testing per virtual host configuration
+# we use the sse-server /health endpoint
+((step+=1))
+echo "[${step}/${total_steps}] Testing per virtual host config true negative"
+check_status "${envoy_url_unfiltered}/health" 200 -H 'Host: foo.vhost-example.com'
+
+((step+=1))
+echo "[${step}/${total_steps}] Testing other wafs do not block per virtual host config true negative"
+check_status "${envoy_url_unfiltered}/health/admin" 404 -H 'Host: foo.vhost-example.com'
+
+((step+=1))
+echo "[${step}/${total_steps}] Testing per virtual host config true positive"
+check_status "${envoy_url_unfiltered}/health/vhost-admin" 403 -H 'Host: foo.vhost-example.com'
+
+((step+=1))
+echo "[${step}/${total_steps}] Testing per virtual host true positive request body"
+check_status "${envoy_url_unfiltered}/health" 403 -X POST -H 'Host: foo.vhost-example.com' -H 'Content-Type: application/x-www-form-urlencoded' --data "evilpayload_vhost"
 
 
 echo "####################################################"
